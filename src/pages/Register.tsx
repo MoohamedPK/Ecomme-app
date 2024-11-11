20005ef6c08a6056c999fcc5ed8a02de2e29de8c
Mohamed <mohamed3atwani2004@gmail.com>
2024-11-11 20:39:56 +0100

    implementing form hook

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,19 +1,35 @@
 import { useForm, SubmitHandler } from "react-hook-form"
 import { Heading } from "@components/common/main";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type TFormInputs = {
-  firstName:string,
-  lastName : string,
-  email:string,
-  password:string,
-  confirmPassword:string
-}
+
+const registerSchema = z.object({
+  firstName: z.string().min(1, {message: "First Name Is Required"}),
+  lastName: z.string().min(1, {message: "Last Name Is Required"}),
+  email: z.string().min(1, {message: "Email Is Required"}).email(),
+  password: z.string().min(8, {message: "Password contains at least 8 characters"})
+
+  // regex for using a special charachters 
+  .regex(/.*[!@#$%^&*( ) _+{ } ] [\]\\:"; '<>?, ./].*/,
+  ),
+  confirmPassword: z.string().min(8, {message: "Confirm Password Is Required"}),
+}).refine(val => val.password === val.confirmPassword, {
+    message: "Your Password Is Not Correct",
+
+    // path allows you to decide where the message display (it takes ARRAY)
+    path: ["confirmPassword"]
+  })
+
+type TFormInputs = z.infer<typeof registerSchema>
 
 function Register() {
 
   // HANDLE SUBMIT prop handle the submit form onClick by passing your function to it as a argument 
-  // REGISTER handle the input data 
-  const {register, handleSubmit} = useForm<TFormInputs>(); 
+  // REGISTER handle the input data (the name="")
+  const {register, handleSubmit, formState:{errors}} = useForm<TFormInputs>({
+    resolver: zodResolver(registerSchema)
+  }); 
 
   const submitForm: SubmitHandler<TFormInputs> = (data) => {
     console.log(data);
@@ -23,10 +39,10 @@ function Register() {
     <>
       <Heading title="Register"/>
 
-          <form action="" className="flex flex-col items-center" onClick={handleSubmit(submitForm)}>
+          <form action="" className="flex flex-col items-center" onSubmit={handleSubmit(submitForm)}>
 
           <label htmlFor="">First Name</label>
-          <input className="my-4 py-1 px-3 border-2 border-neutral-400 rounded-lg w-[300px]" type="text" {...register("firstName")} />
+          <input className={`my-4 py-1 px-3 border-2 border-neutral-400 rounded-lg w-[300px] outline-none ${errors.firstName?.message ? "border-2 border-red-400" : ''}`} type="text" {...register("firstName")}/>
 
           <label htmlFor="">Last Name</label>
           <input className="my-4 py-1 px-3 border-2 border-neutral-400 rounded-lg w-[300px]" type="text" {...register("lastName")} />
